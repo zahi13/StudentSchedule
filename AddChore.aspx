83615eb861f2b277b1ae34cc24bda952d8206036
IraMurachver <iramurachver@gmail.com>
2016-10-31 14:24:31 +0200

    התקדמות עמוד מטלות

--- AddChore.aspx
+++ AddChore.aspx
@@ -4,29 +4,28 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="contentBody" Runat="Server">
     <asp:Table class="AddChoreTable" ID="AddChoreTable" runat="server">
+        
         <asp:TableRow runat="server"> 
-            <asp:TableCell runat="server">Chore Number</asp:TableCell>
-            <asp:TableCell runat="server"><asp:TextBox runat="server" ID="tbCourseName" type="text" /></asp:TableCell>
-        </asp:TableRow>
-        <asp:TableRow runat="server"> 
-            <asp:TableCell runat="server">Chore Name</asp:TableCell>
-            <asp:TableCell runat="server"><asp:TextBox runat="server" id="tbSemester" type="text" /></asp:TableCell>
+            <asp:TableCell runat="server">Course Name</asp:TableCell>
+            <asp:TableCell runat="server"><asp:DropDownList runat="server" id="dd_courseName" AppendDataBoundItems="true">
+                <asp:ListItem Text="choose course" Value="0"/>
+                                          </asp:DropDownList></asp:TableCell>    
         </asp:TableRow>
         <asp:TableRow runat="server"> 
             <asp:TableCell runat="server">Due Date</asp:TableCell>
-            <asp:TableCell runat="server"><asp:TextBox runat="server" id="tbYear" type="text" /></asp:TableCell>
+            <asp:TableCell runat="server"><asp:TextBox runat="server" id="txb_dueDate" type="date" /></asp:TableCell>
         </asp:TableRow>
         <asp:TableRow runat="server"> 
             <asp:TableCell runat="server">Submitted</asp:TableCell>
-            <asp:TableCell runat="server"><asp:TextBox runat="server" id="tbStartDate" type="date" /></asp:TableCell>
+            <asp:TableCell runat="server"><asp:TextBox runat="server" id="txb_submitted" type="date" /></asp:TableCell>
         </asp:TableRow>
         <asp:TableRow runat="server"> 
             <asp:TableCell runat="server">Grade</asp:TableCell>
-            <asp:TableCell runat="server"><asp:TextBox runat="server" id="tbEndDate" type="date" /></asp:TableCell>
+            <asp:TableCell runat="server"><asp:TextBox runat="server" id="txb_grade" type="number" min="0" max="100" value=""/></asp:TableCell>
         </asp:TableRow>
                    
     </asp:Table>
-            <asp:Button ID="btnSubmitChore" runat="server" Text="Submit Chore" OnClick="submitChores"/>
+            <asp:Button ID="btnSubmitChore" runat="server" Text="Submit Chore" OnClick="SubmitChore"/>
             <asp:Label runat="server" ID="lbl_ChoreAdded"></asp:Label>
 
 </asp:Content>
--- AddChore.aspx.cs
+++ AddChore.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +13,87 @@ public partial class AddChore : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        DataTable subjects = new DataTable();
+        using (
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ScheduleDB"].ConnectionString))
+        {
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT courseName FROM Courses", con);
+                adapter.Fill(subjects);
+                dd_courseName.DataSource = subjects;
+                dd_courseName.DataTextField = "courseName";
+                dd_courseName.DataValueField = "courseName";
+                dd_courseName.DataBind();
+            }
+            catch (Exception ex)
+            {
+                ; // Handle the error
+            }
+            finally
+            {
+                con.Close(); //סגירת החיבור
+            }
+        }
 
+    }
+
+
+    protected void SubmitChore(object sender, EventArgs e)
+    {
+        // User s = (User)Session["user"]; //Getting user info from session
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ScheduleDB"].ConnectionString);
+
+        SqlCommand cmd = new SqlCommand(@"insert into Chores values(
+                                            @studentID,
+                                            @courseName,
+                                            @dueDate,
+                                            @Submitted,
+                                            @grade)", con);
+
+        cmd.Parameters.AddWithValue("@studentID", 1);
+        cmd.Parameters.AddWithValue("@courseName", dd_courseName.Text);
+        cmd.Parameters.AddWithValue("@dueDate", DateTime.Now);
+        cmd.Parameters.AddWithValue("@Submitted", DateTime.Now);
+        cmd.Parameters.AddWithValue("@grade", txb_grade.Text);
+
+        int res = 0;
+        try
+        {
+            con.Open();    //פתיחת חיבור בין האתר לבסיס נתונים
+            res = cmd.ExecuteNonQuery();  //ביצוע השאילתא וקליטת הערך לתוך משתנה        
+        }
+        catch (Exception ex)
+        {
+            res = 0; // Handle the error
+        }
+        finally
+        {
+            con.Close(); //סגירת החיבור
+        }
+
+        if (res > 0)
+        {
+            lbl_ChoreAdded.Text = "Chore was added successfully";
+            lbl_ChoreAdded.ForeColor = Color.Green;
+            //reset();
+
+            //Response.Redirect("~/UploadReport.aspx");
+        }
+        else
+        {
+            lbl_ChoreAdded.Text = "Chore was NOT added";
+            lbl_ChoreAdded.ForeColor = Color.Red;
+        }
+
+    }
+
+    protected void reset()
+    {
+        dd_courseName.SelectedIndex = 0;
+        txb_dueDate.Text = null;
+        txb_submitted.Text = null;
+        txb_grade.Text = null;
     }
 }
